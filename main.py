0e0b2c16bd75e6b821f6f57a9b74fd52d7b03010
prabodh-fiddler <prabodh@fiddler.ai>
2023-12-11 23:11:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+import requests
+
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5_V2
+import time
+from PIL import Image, ImageDraw, ImageFont
+import traceback
+
+logging.basicConfig(level=logging.DEBUG)
+
+def get_motivational_quote():
+    # Replace this with your method of fetching a daily motivational quote
+    # Example using an API (change URL and structure based on your chosen API)
+    response = requests.get("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en")
+    if response.status_code == 200:
+        data = response.json()
+        quote = data.get('quoteText', 'Motivational quote not available')
+        return quote
+    else:
+        return 'Failed to fetch quote'
+
+
+def display_content(quote, routine, chess_puzzle):
+    # Create an image with a white background
+    logging.info("epd7in5_V2 Demo")
+    epd = epd7in5_V2.EPD()
+
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear()
+
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+
+    image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
+
+    draw.text((10, 10), quote, font=font18, fill=0)
+
+    epd.display(epd.getbuffer(image))
+    epd.sleep()
+
+
+try:
+    # Get a motivational quote
+    motivational_quote = get_motivational_quote()
+
+    # Define your routine and chess puzzle for the day
+    daily_routine = "Your daily routine goes here."
+    chess_puzzle = "Your chess puzzle goes here."
+
+    # Display content on the e-paper display
+    display_content(motivational_quote, daily_routine, chess_puzzle)
+
+except KeyboardInterrupt:
+    print("Keyboard Interrupt")
+    epd7in5_V2.epdconfig.module_exit()
+    exit()
---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys, os
+from setuptools import setup
+from pathlib import Path
+import re
+
+dependencies = ['Pillow']
+
+def is_raspberry_pi():
+    # https://raspberrypi.stackexchange.com/a/139704/540
+    CPUINFO_PATH = Path("/proc/cpuinfo")
+
+    if not CPUINFO_PATH.exists():
+        return False
+    with open(CPUINFO_PATH) as f:
+        cpuinfo = f.read()
+    return re.search(r"^Model\s*:\s*Raspberry Pi", cpuinfo, flags=re.M) is not None
+
+if is_raspberry_pi():
+    dependencies += ['RPi.GPIO', 'spidev', 'requests']
+elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
+    dependencies += ['Hobot.GPIO', 'spidev', 'requests']
+else:
+    dependencies += ['Jetson.GPIO']
+
+setup(
+    name='waveshare-epd',
+    description='Waveshare e-Paper Display',
+    author='Waveshare',
+    package_dir={'': 'lib'},
+    packages=['waveshare_epd'],
+    install_requires=dependencies,
+)
